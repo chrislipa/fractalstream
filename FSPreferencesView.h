82c7e090b4dcbf6c7ff380d6aaa3c8428ddd37a4
Chris <Chris@lipa>
2012-05-25 19:58:55 -0400

    Create preferences view object.

---
+++ FSPreferencesView.h
@@ -0,0 +1,13 @@
+//
+//  FSPreferencesView.h
+//  FractalStream
+//
+//  Created by Christopher Lipa on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FSPreferencesView : NSPanel
+
+@end
---
+++ FSPreferencesView.m
@@ -0,0 +1,13 @@
+//
+//  FSPreferencesView.m
+//  FractalStream
+//
+//  Created by Christopher Lipa on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FSPreferencesView.h"
+
+@implementation FSPreferencesView
+
+@end
