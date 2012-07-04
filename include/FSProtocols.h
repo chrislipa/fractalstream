38155731c7bae8e0de36c4270b0dcd45e709b6b0
Chris <Chris@lipa>
2012-07-03 17:46:44 -0700

    More refactorin

---
+++ include/FSProtocols.h
@@ -0,0 +1,14 @@
+//
+//  Protocols.h
+//  FractalStream
+//
+//  Created by Christopher Lipa on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FractalStream_Protocols_h
+#define FractalStream_Protocols_h
+
+
+
+#endif
